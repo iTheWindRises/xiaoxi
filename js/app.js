035a08d770c8968409efcae2136bbc0ff1ca753a
wj <425198161@qq.com>
2018-10-16 21:46:05 +0800

    login

---
+++ js/app.js
@@ -0,0 +1,47 @@
+window.app = {
+	/**
+	 * 后端服务器地址
+	 */
+	serverUrl:'http://192.168.2.1:8080',
+	/**
+	 * 图片服务器地址
+	 */
+	ImgserverUrl:'',
+	/**
+	 * 判断字符串是否为空
+	 * @param {Object} str
+	 */
+	isNotNull :function(str) {
+		if(str != null && str != "" && str != undefined) {
+			return true;
+		}
+		
+		return false;
+		
+	},
+	
+	showToast: function(msg,type) {
+		plus.nativeUI.toast(msg,{
+			icon: "image/"+type+".png",
+			verticalAlign:"center"
+		})
+	},
+	
+	/**
+	 * 保存用户全局对象,保存到应用缓存中
+	 * @param {Object} user
+	 */
+	setUserGlobalInfo:function(user) {
+		var userInfoStr = JSON.stringify(user);
+		plus.storage.setItem("userInfo",userInfoStr);
+	},
+	/**
+	 * 
+	 * 获取用户全局对象
+	 */
+	getUserGlobalInfo:function() {
+		var userInfoStr = plus.storage.getItem("userInfo");
+		return JSON.parse(userInfoStr);
+	}
+	
+}
